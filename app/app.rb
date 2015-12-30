d335cd804dadb232524559a57edddafa5067d012
Tony Young <ty_training@hotmail.co.uk>
2015-12-30 23:25:18 +0000

    Added sinatra flash for password confirmation

--- app/app.rb
+++ app/app.rb
@@ -5,12 +5,14 @@ require_relative 'data_mapper_setup'
 require_relative 'models/link'
 require_relative 'models/tag'
 require_relative 'models/user'
+require 'sinatra/flash'
 require 'tilt/erb'
 
 class BManager < Sinatra::Base
 
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   get '/' do
     redirect('/users/new')
@@ -22,8 +24,13 @@ class BManager < Sinatra::Base
 
   post '/users' do
     user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect('/links')
+    if user.save
+      session[:user_id] = user.id
+      redirect('/links')
+    else
+      flash.now[:notice] = 'Password and confirmation password do not match'
+      erb(:'users/new')
+    end
   end
 
   get '/links' do
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,5 +7,7 @@ feature 'user sign up' do
 
   scenario 'requires a matching password for confirmation' do
     expect {sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 end
