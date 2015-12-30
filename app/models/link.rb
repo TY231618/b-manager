8ae973067d8b3540cce9f7898b15f5c8612ff4bf
Tony Young <ty_training@hotmail.co.uk>
2015-12-30 14:19:31 +0000

    Added RACK_ENV for test and development

--- app/models/link.rb
+++ app/models/link.rb
@@ -9,6 +9,6 @@ class Link
   property :title, String
 end
 
-DataMapper.setup(:default, "postgres://localhost/b_manager_test")
+DataMapper.setup(:default, "postgres://localhost/b_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
